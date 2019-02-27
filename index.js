28c82e80d7d8e9520caa86cac8406c5ef4d13760
Daniel Ko <danko56666@gmail.com>
2019-02-26 23:11:45 -0500

    asdasd

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
 
-/* if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     // express will serve up production assets
     // main.js or main.css file
     app.use(express.static("client/build"));
@@ -40,7 +40,7 @@ billingRoutes(app);
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
-} */
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
